76c327b07307a85b6d90eb04535d28848388d593
pineapplebrooks <127373042+pineapplebrooks@users.noreply.github.com>
2025-04-25 00:08:09 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Happy developing ✨')
+function createDates(month, numDays, counter) {
+
+    const cont = document.getElementById(month);
+
+    let count = counter;
+
+    for (let x = 2; x <= numDays; x++) {
+
+        let date = document.createElement("p");
+        date.setAttribute("class", "day");
+        date.setAttribute("style", "grid-area: " + count + " / 1");
+        date.textContent = "" + x + "";
+
+        cont.appendChild(date);
+        count++;
+    }
+}
+
+createDates("may", 31, 7);
+createDates("june", 30, 2);
+createDates("july", 31, 2);
+createDates("august", 31, 2);
